1c499fb9d6ef72e856c7916e3f1ef64b21fe488a
Bohdan Vorobiei <vbs@ukr.net>
2026-04-15 02:22:03 +0300

    test13

--- src/main/java/mate/academy/hibernate/relations/dao/impl/AbstractDao.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/AbstractDao.java
@@ -5,7 +5,8 @@ import org.hibernate.SessionFactory;
 public abstract class AbstractDao {
     protected final SessionFactory factory;
 
-    protected AbstractDao(SessionFactory sessionFactory) {
-        this.factory = sessionFactory;
+    public AbstractDao(SessionFactory factory) {
+        this.factory = factory;
     }
 }
+
--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -1,12 +1,13 @@
 package mate.academy.hibernate.relations.dao.impl;
 
-import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+
+import java.util.Optional;
 
 public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
